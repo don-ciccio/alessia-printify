6b049151af2f51d07eff3d95a8efd2b81a7e1c20
Danilo D'Alessio <danilo.dalessio@gmail.com>
2024-04-20 12:35:12 +0200

    scaffolding with dummy controllers

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+import appSetup from "./startup/init";
+import routerSetup from "./startup/router";
+import securitySetup from "./startup/security";
+import dotenv from "dotenv";
+dotenv.config();
+
+appSetup(app);
+securitySetup(app, express);
+routerSetup(app);
